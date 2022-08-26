6e1fd365a1dd4039dac98d3258bad96194a432cb
mhug <maettuh@gmail.com>
2022-08-26 11:56:15 +0200

    refactored configuration

--- GW2AutoLogin/Program.cs
+++ GW2AutoLogin/Program.cs
@@ -1,67 +1,91 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using WindowsInput;
 using WindowsInput.Native;
 
-namespace GW2AutoLogin
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
+namespace GW2AutoLogin {
 
-            try
-            {
+    class Program {
+        private const string PropInstallDir = "installDir";
+        private const string PropWaitTime = "waitTime";
+        private const string PropAccount = "account";
 
+        public static void Main(string[] args) {
 
-                string[] lines = System.IO.File.ReadAllLines(Directory.GetCurrentDirectory()  + @"\config.txt");
-
-                string path = lines[0];
-                //int timeOut = int.Parse(lines[1]);
-
+            try {
+                Config config = initConfig();
 
                 Process firstProc = new Process();
 
                 firstProc.StartInfo.UseShellExecute = true;
-                firstProc.StartInfo.WorkingDirectory = path;
+                firstProc.StartInfo.WorkingDirectory = config.installDir;
                 firstProc.StartInfo.FileName = "Gw2-64";
                 firstProc.EnableRaisingEvents = true;
 
-                for (int i =2; i < lines.Length; i+=2)
-                {
-                    firstProc.Start();
-                    System.Threading.Thread.Sleep(10 * 1000);
-                    InputSimulator s = new InputSimulator();
-
-                    s.Keyboard.TextEntry(lines[i]);
-
-
-
-                    s.Keyboard.KeyPress(VirtualKeyCode.TAB);
-
-                    s.Keyboard.TextEntry(lines[i+1]);
-
-                    s.Keyboard.KeyPress(VirtualKeyCode.RETURN);
-
-                    System.Threading.Thread.Sleep(10 * 1000);
-                    s.Keyboard.KeyPress(VirtualKeyCode.RETURN);
-
-                    System.Threading.Thread.Sleep(10 * 1000);
-                    s.Keyboard.KeyPress(VirtualKeyCode.RETURN);
-
-                    System.Threading.Thread.Sleep(30 * 1000);
-
-
-                    firstProc.Kill();
+                foreach (var credentials in config.credentials) {
+                    runAutoLogin(firstProc, config, credentials);
                 }
 
-            }
-            catch (Exception ex)
-            {
+            } catch (Exception ex) {
                 Console.WriteLine("An error occurred!!!: " + ex.Message);
                 return;
             }
         }
+
+        private static void runAutoLogin(Process process, Config config, KeyValuePair<string, string> credentials) {
+            process.Start();
+            System.Threading.Thread.Sleep(10 * 1000);
+            InputSimulator s = new InputSimulator();
+
+            s.Keyboard.TextEntry(credentials.Key);
+
+            s.Keyboard.KeyPress(VirtualKeyCode.TAB);
+
+            s.Keyboard.TextEntry(credentials.Value);
+
+            s.Keyboard.KeyPress(VirtualKeyCode.RETURN);
+
+            System.Threading.Thread.Sleep(10 * 1000);
+            s.Keyboard.KeyPress(VirtualKeyCode.RETURN);
+
+            System.Threading.Thread.Sleep(10 * 1000);
+            s.Keyboard.KeyPress(VirtualKeyCode.RETURN);
+
+            System.Threading.Thread.Sleep(config.waitTime);
+
+            process.Kill();
+        }
+
+        private static Config initConfig() {
+
+            string[] lines = System.IO.File.ReadAllLines(Directory.GetCurrentDirectory() + @"\config.txt");
+            Config config = new Config();
+
+            for (int i = 0; i < lines.Length; i++) {
+                if (lines[i].StartsWith(PropInstallDir)) {
+                    config.installDir = lines[i].Substring(PropInstallDir.Length + 1);
+                }
+                if (lines[i].StartsWith(PropWaitTime)) {
+                    config.waitTime = int.Parse(lines[i].Substring(PropWaitTime.Length + 1));
+                }
+
+                if (lines[i].StartsWith(PropAccount)) {
+                    string[] accInfo = lines[i].Substring(PropAccount.Length + 1).Split(" ");
+                    config.credentials.Add(accInfo[0], accInfo[1]);
+                }
+            }
+
+            return config;
+        }
+    }
+
+    class Config {
+        public string installDir { get; set; }
+        public int waitTime { get; set; }
+
+        private IDictionary<string, string> _credentials = new Dictionary<string, string>();
+        public IDictionary<string, string> credentials => _credentials;
     }
 }
